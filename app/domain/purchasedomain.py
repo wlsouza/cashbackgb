47988c77b7e966761689d9a716e9eab2d8a57af3
Wellington Lorenço <wlsouza@protonmail.com>
2022-01-22 20:03:46 -0300

    feat: add method in purchasedomain to get purchase status

--- app/domain/purchasedomain.py
+++ app/domain/purchasedomain.py
@@ -1,5 +1,10 @@
 from decimal import Decimal
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app import crud
+
+
 class PurchaseDomain:
     """
     Class responsible for purchasing business rules
@@ -26,6 +31,33 @@ class PurchaseDomain:
         if purchase_value <= 1500:
             return purchase_value * Decimal(0.15)
         return purchase_value * Decimal(0.2)
+
+
+    async def get_default_purchase_status_id(self,db:AsyncSession, *, purchase_user_id:int) -> int:
+        """
+        Method that get correct default purchase status for a new purchase.
+        Rule:
+            - Every new purchase must have the status "In validation", except
+            when the user's CPF is '15350946056', in this case the status 
+            must be "Approved"
+        Args:
+            purchase_user_id (int): id of purchase user.
+            db (AsyncSession): Database async session.
+
+        Returns:
+            int: id of status.
+        """        
+        print("oi")
+        user = await crud.user.get_by_id(db=db, id = purchase_user_id)
+        if user.cpf == "15350946056":
+            status = await crud.purchase_status.get_by_name(
+                db=db, name="Approved"
+            )
+        else:
+            status = await crud.purchase_status.get_by_name(
+                db=db, name="In validation"
+            )
+        return status.id
 
 
 purchase = PurchaseDomain()

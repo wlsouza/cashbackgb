ea4b24658121422539debb140f943eacba69801a
Wellington Lorenço <wlsouza@protonmail.com>
2022-01-23 13:58:56 -0300

    feat[crud.cruduser]: add method to get authenticated user

--- app/tests/crud/test_crud_user.py
+++ app/tests/crud/test_crud_user.py
@@ -1,7 +1,8 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud, schemas
+from app import crud, schemas, models
+from app.core.security import verify_password
 from app.tests.utils.user import (
     fake,
     random_user_dict,
@@ -31,6 +32,16 @@ async def test_when_create_user_return_hashed_password(
     user_in = schemas.UserCreate(**user_dict)
     new_user = await crud.user.create(db=db, user_in=user_in)
     assert hasattr(new_user, "hashed_password")
+
+@pytest.mark.asyncio
+async def test_when_create_user_return_valid_hashed_password(
+    db: AsyncSession,
+) -> None:
+    user_dict = random_user_dict()
+    user_in = schemas.UserCreate(**user_dict)
+    new_user = await crud.user.create(db=db, user_in=user_in)
+    result = verify_password(user_dict["password"], new_user.hashed_password)
+    assert result
 
 @pytest.mark.asyncio
 async def test_if_get_by_email_return_correct_user(db: AsyncSession) -> None:
@@ -125,3 +136,43 @@ async def test_if_get_multi_skip_the_correct_quantity_of_user(
     db_users = await crud.user.get_multi(db=db, limit=5)
     users = await crud.user.get_multi(db=db, skip=2, limit=1)
     assert users[0].id == db_users[2].id
+
+
+@pytest.mark.asyncio
+async def test_when_successfully_get_authenticated_user_must_return_user(
+    db: AsyncSession,
+) -> None:
+    user_dict = random_user_dict()
+    await crud.user.create(db=db, user_in=user_dict)
+    result = await crud.user.get_authenticated_user(
+        db=db,
+        user_email=user_dict["email"],
+        user_password=user_dict["password"]
+    )
+    assert isinstance(result, models.User)
+
+@pytest.mark.asyncio
+async def test_when_getting_authenticated_user_if_invalid_email_must_return_none(
+    db: AsyncSession,
+) -> None:
+    user_dict = random_user_dict()
+    await crud.user.create(db=db, user_in=user_dict)
+    result = await crud.user.get_authenticated_user(
+        db=db,
+        user_email="invalid_email@test.com",
+        user_password=user_dict["password"]
+    )
+    assert result is None
+
+@pytest.mark.asyncio
+async def test_when_getting_authenticated_user_if_invalid_password_must_return_none(
+    db: AsyncSession,
+) -> None:
+    user_dict = random_user_dict()
+    await crud.user.create(db=db, user_in=user_dict)
+    result = await crud.user.get_authenticated_user(
+        db=db,
+        user_email=user_dict["email"],
+        user_password="invalid_password_test"
+    )
+    assert result is None

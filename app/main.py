02686c5815d4b4444db7635c963e0d9a0bfff9e9
Wellington Lorenço <wlsouza@protonmail.com>
2022-01-25 18:35:56 -0300

    ci: add mangum wrapper to deploy the app in lambda

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.api.api_v1.api import api_v1_router
 from app.core.config import settings
@@ -13,3 +14,6 @@ app.include_router(api_v1_router, prefix=settings.API_V1_STR)
 @app.get("/")
 def im_alive() -> Any:
     return "Hi, I'm alive!"
+
+handler = Mangum(app)
+
